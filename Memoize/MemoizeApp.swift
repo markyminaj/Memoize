7b77582eda90e9c7baf0b8eb4f22c4697ba60ca4
Mark <menm619@gmail.com>
2022-02-03 13:14:20 -0800

    Initial Commit

---
+++ Memoize/MemoizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoizeApp.swift
+//  Memoize
+//
+//  Created by Mark Martin on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
